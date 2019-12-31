647495f5a67fbd30f4016a5404847429deab3e46
template <sskvortsovs@mail.ru>
2019-12-31 09:11:31 +0300

    webpack
    connected webpack

--- script/index.js
+++ script/index.js
@@ -16,7 +16,7 @@ function getFrontPage(){
     logo.style.marginLeft = '0';
     containerLogo.style.marginTop = '0';
     body.style.backgroundColor = '#202020';
-    containerGolden.style.border = '1px solid #ce9f51';
+    containerGolden.style.border = '2px solid #ce9f51';
     containerGolden.style.minHeight = '97vh';
     logo.style.opacity = '1';
     logo.style.marginLeft = '5px';
@@ -34,13 +34,20 @@ function loadWritesFrontPage(){
     let informationDescription = document.createElement('p');
 
     informationContainer.style.marginTop = '15%';
+    informationHeader.classList.add('information-header');
+    informationHeader.style.marginLeft = '30px';
+    informationHeader.style.marginBottom = 'auto';
+    informationHeader.style.width = '100%';
     informationHeader.style.fontFamily = 'Caveat, cursive';
     informationHeader.style.color = '#ce9f51';
-    informationHeader.style.fontSize = '50px';
     informationHeader.textContent = 'Инвестирйте в своё время';
     informationHeader.setAttribute('data-aos', 'fade-up');
     informationHeader.setAttribute('data-aos-delay', '10000');
+    informationDescription.style.marginLeft = '30px';
+    informationDescription.style.fontSize = '20px';
+    informationDescription.style.width = '60%';
     informationDescription.style.color = 'gray';
+    informationDescription.style.fontFamily = 'Montserrat';
     informationDescription.textContent = informationDescriptionText;
 
     informationContainer.appendChild(informationHeader);
