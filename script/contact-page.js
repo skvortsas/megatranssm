91534c1a313f0dd99773590badc372c4bc6b9978
Stanislav <fskvortsovf@gmail.com>
2020-02-03 22:47:23 +0300

    contact page
    added form to contact page;
    added some style to it;
    added content to form note

--- script/contact-page.js
+++ script/contact-page.js
@@ -9,6 +9,11 @@ let phoneTransport = document.createElement('p');
 let mailHeader = document.createElement('p');
 let email = document.createElement('p');
 let formContainer = document.createElement('span');
+let formNote = document.createElement('p');
+let nameField = document.createElement('input');
+let phoneField = document.createElement('input');
+let emailField = document.createElement('input');
+let sendButton = document.createElement('button');
 
 export default function contactPage(mainContainer, pages, pageNumber){
     let note = document.getElementsByClassName('advantage-note')[0];
@@ -27,12 +32,62 @@ export default function contactPage(mainContainer, pages, pageNumber){
         advantage.parentNode.removeChild(advantage);
     }
 
+    //adding contact information(left side)
+    addContactInformation(pages, pageNumber);
+    //adding form for contacting
+    addFormContent(pages, pageNumber);
+
     mainContainer.style.marginLeft = '0';
     mainContainer.style.marginRight = '0';
     mainContainer.style.marginTop = '0';
 
+    //merging all together
+    contactInformation.appendChild(informationContainer);
+    contactInformation.appendChild(formContainer);
+    mainContainer.appendChild(contactInformation);
+}
+
+function addFormContent(pages, pageNumber){
+    //adding stylings
+    formNote.classList.add('form-note');
+    nameField.classList.add('form-input');
+    phoneField.classList.add('form-input');
+    emailField.classList.add('form-input');
+    sendButton.classList.add('submit-button');
+
+    //adding the content
+    formNote.textContent = pages[pageNumber].formNote;
+    nameField.placeholder = 'Имя';
+    vanishPlaceholder(nameField, 'Имя');
+    phoneField.placeholder = 'Телефон';
+    vanishPlaceholder(phoneField, 'Телефон');
+    emailField.placeholder = 'E-mail';
+    vanishPlaceholder(emailField, 'E-mail');
+    sendButton.textContent = 'Отправить';
+
+    //append all elements
+    formContainer.appendChild(formNote);
+    formContainer.appendChild(nameField);
+    formContainer.appendChild(phoneField);
+    formContainer.appendChild(emailField);
+    formContainer.appendChild(sendButton);
+}
+
+function vanishPlaceholder(element, placeholderContent){
+    element.onfocus = () => {
+        element.placeholder = '';
+    };
+
+    element.onblur = () => {
+        element.placeholder = placeholderContent;
+    };
+}
+
+function addContactInformation(pages, pageNumber){
     //adding stylings
     contactInformation.classList.add('contact-information');
+    contactInformation.setAttribute('data-aos', 'fade-up');
+    contactInformation.setAttribute('data-aos-duration', 1000);
     informationContainer.classList.add('contact-information-container');
     formContainer.classList.add('form-container');
     company.classList.add('company');
@@ -62,7 +117,4 @@ export default function contactPage(mainContainer, pages, pageNumber){
     informationContainer.appendChild(phoneContainer);
     informationContainer.appendChild(mailHeader);
     informationContainer.appendChild(email);
-    contactInformation.appendChild(informationContainer);
-    contactInformation.appendChild(formContainer);
-    mainContainer.appendChild(contactInformation);
 }
